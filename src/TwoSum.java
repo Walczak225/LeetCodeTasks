eab3ab553bf9b11c5f5483b3d6d46ee5db84f05e
Walczak225 <mateusz.walczak225@gmail.com>
2026-04-22 10:40:31 +0200

    PalindromeNumber done

--- src/TwoSum.java
+++ src/TwoSum.java
@@ -1,17 +1,19 @@
 import java.util.HashMap;
 import java.util.Map;
 
-public class TwoSum {
-    public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> prevMap = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            int num = nums[i];
-            int diff = target - num;
-            if (prevMap.containsKey(diff)) {
-                return new int[] { prevMap.get(diff), i };
+void main() {
+     class TwoSum {
+        public int[] twoSum(int[] nums, int target) {
+            Map<Integer, Integer> prevMap = new HashMap<>();
+            for (int i = 0; i < nums.length; i++) {
+                int num = nums[i];
+                int diff = target - num;
+                if (prevMap.containsKey(diff)) {
+                    return new int[]{prevMap.get(diff), i};
+                }
+                prevMap.put(num, i);
             }
-            prevMap.put(num, i);
+            return new int[]{};
         }
-        return new int[] {};
     }
 }
